64093f38d445c8aecafd7d162aed2b0a5499d1e6
Damien Abeloos <dabeloos@w-r3-p3.s19.be>
2018-08-07 17:16:21 +0200

    correction fin r_dwssap.sh ex07

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v '^#' | awk 'NR%2' | cut -d : -f 1 | rev | sort -r | awk 'FT_LINE1<=NR&&FT_LINE2>=NR'  | tr '\n' ',' | sed '$s/,$/./' | sed 's/,/, /g'
+cat /etc/passwd | grep -v '^#' | awk 'NR%2' | cut -d : -f 1 | rev | sort -r | awk "$FT_LINE1<=NR&&$FT_LINE2>=NR"  | tr '\n' ',' | sed '$s/,$/./' | sed 's/,/, /g'
